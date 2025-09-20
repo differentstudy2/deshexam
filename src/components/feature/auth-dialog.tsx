587f970ce6f45e2f9191e61579bfe35796727ecd
Jahanur Miah <starjahanur123@gmail.com>
2025-09-20 16:28:50 +0000

    display google icon also in log in and sign up

--- src/components/feature/auth-dialog.tsx
+++ src/components/feature/auth-dialog.tsx
@@ -38,6 +38,16 @@ type SignInValues = z.infer<typeof signInSchema>;
 type SignUpValues = z.infer<typeof signUpSchema>;
 
 
+const GoogleIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" width="24px" height="24px" {...props}>
+        <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z" />
+        <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z" />
+        <path fill="#4CAF50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36c-5.222,0-9.565-3.108-11.303-7.524l-6.571,4.819C9.656,39.663,16.318,44,24,44z" />
+        <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.574l6.19,5.238C39.914,34.062,44,28.868,44,24C44,22.659,43.862,21.35,43.611,20.083z" />
+    </svg>
+);
+
+
 // Sign In Form Component
 const SignInForm = () => {
   const { signIn, signInWithGoogle } = useAuth();
@@ -105,6 +115,7 @@ const SignInForm = () => {
             {form.formState.isSubmitting ? "Logging In..." : "Login"}
           </Button>
           <Button variant="outline" className="w-full" type="button" onClick={handleGoogleSignIn}>
+            <GoogleIcon className="mr-2 h-4 w-4" />
             Login with Google
           </Button>
         </form>
@@ -171,6 +182,7 @@ const SignUpForm = () => {
             {form.formState.isSubmitting ? "Creating Account..." : "Create an account"}
           </Button>
           <Button variant="outline" className="w-full" type="button" onClick={handleGoogleSignIn}>
+            <GoogleIcon className="mr-2 h-4 w-4" />
             Sign up with Google
           </Button>
         </form>
