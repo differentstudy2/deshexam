d237c67069aaee59d93822a15b2fd28fa4befe70
Jahanur Miah <starjahanur123@gmail.com>
2025-09-22 07:54:31 +0000

    no it is does not solved

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,8 +1,13 @@
-// DO NOT EDIT
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
-// DO NOT EDIT
 
+// Give the service worker access to Firebase Messaging.
+// Note that you can only use Firebase Messaging here, other Firebase libraries
+// are not available in the service worker.
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging-compat.js');
+
+// Initialize the Firebase app in the service worker by passing in
+// your app's Firebase config object.
+// https://firebase.google.com/docs/web/setup#config-object
 const firebaseConfig = {
   apiKey: "AIzaSyAzSss2t5UoydcGRh4CJ41VvE4x-t0Ikrc",
   authDomain: "studio-8356746366-699c1.firebaseapp.com",
@@ -14,22 +19,18 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
+// Retrieve an instance of Firebase Messaging so that it can handle background
+// messages.
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage((payload) => {
-  console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  
-  // Exit early if a client is already focused.
-  // This prevents double notifications.
-  if (self.clients.focused) {
-    return;
-  }
-  
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: '/icon.png'
-  };
+// If you would like to customize notifications that are received in the
+// background (Web app is closed or not in browser focus) then you should
+// implement this optional method.
+// Keep in mind that FCM will still show notification messages automatically 
+// and only data messages will be handled by this method.
+// Refer to the online documentation for more information.
+// https://firebase.google.com/docs/cloud-messaging/js/receive
 
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
+// This service worker is intentionally left blank for background message handling.
+// The default FCM behavior will display notifications when the app is in the background.
+// Foreground messages are handled in src/app/layout.tsx to display a toast.
