91d96eb521089a4dfe9afbe8de5ea0bfdf869c0a
Jahanur Miah <starjahanur123@gmail.com>
2025-09-22 07:45:58 +0000

    its seen double double

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,10 +1,8 @@
+// DO NOT EDIT
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
+// DO NOT EDIT
 
-// Give the service worker access to Firebase Messaging.
-// Note: this lives in the public folder
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
-
-// Initialize the Firebase app in the service worker with your Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAzSss2t5UoydcGRh4CJ41VvE4x-t0Ikrc",
   authDomain: "studio-8356746366-699c1.firebaseapp.com",
@@ -16,12 +14,17 @@ const firebaseConfig = {
 
 firebase.initializeApp(firebaseConfig);
 
-// Retrieve an instance of Firebase Messaging so that it can handle background messages.
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage((payload) => {
   console.log('[firebase-messaging-sw.js] Received background message ', payload);
-  // Customize notification here
+  
+  // Exit early if a client is already focused.
+  // This prevents double notifications.
+  if (self.clients.focused) {
+    return;
+  }
+  
   const notificationTitle = payload.notification.title;
   const notificationOptions = {
     body: payload.notification.body,
