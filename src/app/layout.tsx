990cd6b49eb3df732fef603479a138755ff313a6
Jahanur Miah <starjahanur123@gmail.com>
2025-09-22 06:29:04 +0000

    now the question is where user get that notification?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,7 +14,7 @@ import { AuthDialog } from "@/components/feature/auth-dialog";
 import { usePathname } from "next/navigation";
 import { useEffect } from "react";
 import { messaging } from "@/lib/firebase/client";
-import { getToken } from "firebase/messaging";
+import { getToken, onMessage } from "firebase/messaging";
 import { useToast } from "@/hooks/use-toast";
 import { addFCMToken } from "@/lib/firebase/firestore";
 
@@ -31,6 +31,16 @@ const FirebaseMessagingProvider = ({ children }: { children: React.ReactNode }) 
             const currentToken = await getToken(messaging, { vapidKey: 'BPwG_wGjDhWT_DVSY3Fd6fgrhNhlQrK2hklIDuqFRu4B29XTRnOemSrulxX0RAFXtzjy2dg0a0EV34RkK48CakA' });
             if (currentToken) {
               await addFCMToken(currentToken);
+              
+              // Listen for foreground messages
+              onMessage(messaging, (payload) => {
+                console.log('Message received. ', payload);
+                toast({
+                  title: payload.notification?.title,
+                  description: payload.notification?.body,
+                });
+              });
+
             } else {
               console.log('No registration token available. Request permission to generate one.');
             }
