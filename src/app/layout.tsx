d685756eda1307ff0f7416193610e43f0381ba78
Jahanur Miah <starjahanur123@gmail.com>
2025-10-03 09:05:31 +0000

    hey see this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,11 +12,6 @@ import { ThemeProvider } from "@/components/layout/theme-provider";
 import { AuthDialogProvider } from "@/hooks/use-auth-dialog";
 import { AuthDialog } from "@/components/feature/auth-dialog";
 import { usePathname } from "next/navigation";
-import { useEffect } from "react";
-import { messaging } from "@/lib/firebase/client";
-import { getToken } from "firebase/messaging";
-import { useToast } from "@/hooks/use-toast";
-import { addFCMToken } from "@/lib/firebase/firestore";
 import { Inter, Lexend } from 'next/font/google';
 
 const inter = Inter({
@@ -29,43 +24,6 @@ const lexend = Lexend({
   variable: '--font-lexend',
   weight: ['400', '700'],
 });
-
-
-const FirebaseMessagingProvider = ({ children }: { children: React.ReactNode }) => {
-  const { toast } = useToast();
-  const { user } = useAuth();
-
-  useEffect(() => {
-    const requestPermission = async () => {
-      if (typeof window !== 'undefined' && 'serviceWorker' in navigator && messaging) {
-        try {
-          const permission = await Notification.requestPermission();
-          if (permission === 'granted') {
-            const currentToken = await getToken(messaging, { vapidKey: 'BPwG_wGjDhWT_DVSY3Fd6fgrhNhlQrK2hklIDuqFRu4B29XTRnOemSrulxX0RAFXtzjy2dg0a0EV34RkK48CakA' });
-            if (currentToken) {
-              await addFCMToken(currentToken);
-            } else {
-              console.log('No registration token available. Request permission to generate one.');
-            }
-          }
-        } catch (err) {
-          console.error('An error occurred while retrieving token. ', err);
-          toast({
-            variant: "destructive",
-            title: "Notification Error",
-            description: "Could not enable push notifications.",
-          });
-        }
-      }
-    };
-    
-    if (user) {
-      requestPermission();
-    }
-  }, [toast, user]);
-
-  return <>{children}</>;
-};
 
 export default function RootLayout({
   children,
@@ -86,14 +44,12 @@ export default function RootLayout({
         >
           <AuthProvider>
             <AuthDialogProvider>
-             <FirebaseMessagingProvider>
               <div className="flex flex-col min-h-screen">
                 <Header />
                 <main className="flex-grow">{children}</main>
                 {showFooter && <Footer />}
               </div>
               <AuthDialog />
-              </FirebaseMessagingProvider>
             </AuthDialogProvider>
           </AuthProvider>
           <Toaster />
