586e8260fbca4a3d853d5515233470b100e0af16
Jahanur Miah <starjahanur123@gmail.com>
2025-10-29 21:22:34 +0000

    add fix  width for chapter icon

--- src/app/textbook-solutions/[bookId]/chapter/[chapterId]/chapter-client-page.tsx
+++ src/app/textbook-solutions/[bookId]/chapter/[chapterId]/chapter-client-page.tsx
@@ -41,7 +41,7 @@ const getResourceIcon = (type: string) => {
 };
 
 const ChapterIcon = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="h-5 w-5 flex-shrink-0"><path d="M12 7v14"></path><path d="M3 18a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h5a4 4 0 0 1 4 4 4 4 0 0 1 4-4h5a1 1 0 0 1 1 1v13a1 1 0 0 1-1 1h-6a3 3 0 0 0-3 3 3 3 0 0 0-3-3z"></path></svg>
 );
 
 const ExamIcon = () => (
@@ -540,3 +540,4 @@ export default function ChapterClientPage() {
         </div>
     );
 }
+
