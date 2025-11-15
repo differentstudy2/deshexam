6732d3f633da224e86249bf48b1dcb081c3c14a7
Jahanur Miah <starjahanur123@gmail.com>
2025-11-15 23:08:14 +0000

    Error: Invalid src prop (https://archive.org/download/selectedpoemshayes

--- next.config.ts
+++ next.config.ts
@@ -53,6 +53,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'archive.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
   async rewrites() {
