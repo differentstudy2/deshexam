aa593e5b432f95ed9ce78bfda1e19b902928999e
Jahanur Miah <starjahanur123@gmail.com>
2025-12-23 23:55:07 +0000

    this get 404 /textbook-solutions/id/practice-set/id but this /textbook-s

--- src/next.config.ts
+++ src/next.config.ts
@@ -79,6 +79,10 @@ const nextConfig: NextConfig = {
         source: '/textbook-solutions/:bookId/exam/:examId',
         destination: '/textbook-solutions/exam/:examId/textbook/:bookId',
       },
+       {
+        source: '/textbook-solutions/:bookId/practice-set/:practiceSetId',
+        destination: '/textbook-solutions/practice-set/:practiceSetId/textbook/:bookId',
+      },
       {
         source: '/textbook-solutions/:bookId/quiz/:quizId',
         destination: '/textbook-solutions/quiz/:quizId/textbook/:bookId',
@@ -88,16 +92,8 @@ const nextConfig: NextConfig = {
         destination: '/content/:id*',
       },
       {
-        source: '/textbook-solutions/practice-set/:practiceSetId/textbook/:bookId',
-        destination: '/textbook-solutions/practice-set/:practiceSetId/textbook/:bookId/chapter/null/topic/null',
-      },
-      {
         source: '/textbook-solutions/mock-test/:mockTestId/textbook/:bookId/chapter/:chapterId/topic/:topicId',
         destination: '/textbook-solutions/mock-test/:mockTestId/textbook/:bookId/chapter/:chapterId/topic/:topicId',
-      },
-       {
-        source: '/textbook-solutions/:bookId/practice-set/:practiceSetId',
-        destination: '/textbook-solutions/practice-set/:practiceSetId/textbook/:bookId/chapter/null/topic/null',
       },
     ];
   }
