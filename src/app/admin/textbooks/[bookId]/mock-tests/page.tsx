83b3b532f810cc8a77a055a5aba46c2946cd4054
Jahanur Miah <starjahanur123@gmail.com>
2025-12-23 17:47:56 +0000

    remove Subject collumn

--- src/app/admin/textbooks/[bookId]/mock-tests/page.tsx
+++ src/app/admin/textbooks/[bookId]/mock-tests/page.tsx
@@ -217,7 +217,6 @@ export default function ManageTextbookMockTestsPage() {
                             <TableRow>
                                 <TableHead className="w-20">Image</TableHead>
                                 <TableHead>Title</TableHead>
-                                <TableHead>Subject</TableHead>
                                 <TableHead>Questions</TableHead>
                                 <TableHead>Access</TableHead>
                                 <TableHead className="text-right">Actions</TableHead>
@@ -229,7 +228,6 @@ export default function ManageTextbookMockTestsPage() {
                                 <TableRow key={i}>
                                     <TableCell><Skeleton className="h-10 w-16 rounded-md" /></TableCell>
                                     <TableCell><Skeleton className="h-5 w-3/4" /></TableCell>
-                                    <TableCell><Skeleton className="h-5 w-24" /></TableCell>
                                     <TableCell><Skeleton className="h-5 w-12" /></TableCell>
                                     <TableCell><Skeleton className="h-5 w-16" /></TableCell>
                                     <TableCell className="text-right"><Skeleton className="h-8 w-32 ml-auto" /></TableCell>
@@ -248,7 +246,6 @@ export default function ManageTextbookMockTestsPage() {
                                         />
                                     </TableCell>
                                     <TableCell className="font-medium">{test.subtitle ? `${test.subtitle}: ${test.title}` : test.title}</TableCell>
-                                    <TableCell>{test.subject}</TableCell>
                                     <TableCell>{test.questions?.length || 0}</TableCell>
                                     <TableCell><ContentBadge type={test.access} /></TableCell>
                                     <TableCell className="text-right space-x-2">
@@ -268,7 +265,7 @@ export default function ManageTextbookMockTestsPage() {
                                 </TableRow>
                             ))) : (
                                 <TableRow>
-                                    <TableCell colSpan={6} className="text-center h-24">
+                                    <TableCell colSpan={5} className="text-center h-24">
                                     No mock tests added to this textbook yet.
                                     </TableCell>
                                 </TableRow>
@@ -360,7 +357,6 @@ export default function ManageTextbookMockTestsPage() {
                     </DialogFooter>
                 </DialogContent>
             </Dialog>
-
 
             <AlertDialog open={!!testToDelete} onOpenChange={() => setTestToDelete(null)}>
                 <AlertDialogContent>
