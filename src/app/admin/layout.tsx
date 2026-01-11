504496eabf510371b8e80741c1641dbe994b6b04
Jahanur Miah <starjahanur123@gmail.com>
2026-01-11 11:33:44 +0000

    there is double footers

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -73,7 +73,6 @@ export default function AdminLayout({
                         {children}
                     </main>
                 </SidebarInset>
-                <Footer />
             </div>
         </div>
     </SidebarProvider>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -49,7 +49,6 @@ export default function DashboardLayout({
                         {children}
                     </main>
                 </SidebarInset>
-                <Footer />
             </div>
         </div>
     </SidebarProvider>
