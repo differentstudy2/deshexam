6b6defa52db980094bcb237654e68f7ee8e5970c
Jahanur Miah <starjahanur123@gmail.com>
2026-03-11 15:57:31 +0000

    Try fixing this error: `Runtime Error: Error: React.Children.only expect

--- src/app/admin/kids-zone/edit/[id]/page.tsx
+++ src/app/admin/kids-zone/edit/[id]/page.tsx
@@ -4,6 +4,8 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 
+// This page is deprecated. The functionality has been moved to a dialog
+// in `src/app/admin/kids-zone/manage/page.tsx`.
 export default function DeprecatedEditKidsContentPage() {
     const router = useRouter();
     useEffect(() => {
