6a1a71bdc6b78867b4894868ad662101045a8b74
mdjmiah1 <starjahanur123@gmail.com>
2026-07-19 22:52:06 +0530

    feat: implement interactive PresentationOverlay for assessment-center slide navigation and display

--- src/app/[locale]/admin/assessment-center/[type]/[slug]/answer-sheet/PresentationOverlay.tsx
+++ src/app/[locale]/admin/assessment-center/[type]/[slug]/answer-sheet/PresentationOverlay.tsx
@@ -163,7 +163,7 @@ export default function PresentationOverlay({ questions, classLine, chapterName,
                 </div>
 
                 {/* Foreground Watermark */}
-                <div className="absolute top-[40%] left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-12 text-[120px] font-black text-gray-200/50 tracking-widest uppercase z-30 pointer-events-none select-none">
+                <div className="absolute top-[75%] left-1/2 -translate-x-1/2 -translate-y-1/2 -rotate-[8deg] text-[100px] font-black text-gray-200/50 tracking-widest uppercase z-30 pointer-events-none select-none">
                     DESHEXAM
                 </div>
 
