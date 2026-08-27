c1d65d6055f670bd81b9788a88fee270bd1af385
mdjmiah1 <starjahanur123@gmail.com>
2026-08-27 13:33:03 +0530

    style: remove obsolete styles.css file

--- src/app/blog/[id]/page.tsx
+++
@@ -1,5 +0,0 @@
-import Page, { generateMetadata } from '../../content/[id]/page';
-
-export { /* @next-codemod-error `generateMetadata` export is re-exported. Check if this component uses `params` or `searchParams`*/
-generateMetadata };
-export default Page;
